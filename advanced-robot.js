76abca6be5d03aae1c9455c60f009a2e03e68c07
Carlos Hernández Gómez <karliky@gmail.com>
2018-07-01 19:13:44 +0200

    Add files via upload

---
+++ advanced-robot.js
@@ -0,0 +1,26 @@
+
+  var Robot = function() {
+    console.log('Advanceddd!!!')
+  };
+
+  Robot.prototype.onIdle = function(robot) {
+    robot.ahead(20);
+    robot.turn(20);
+    robot.rotateCannon(360);
+  };
+
+  Robot.prototype.onScannedRobot = function(robot) {
+    robot.fire();
+  };
+
+  Robot.prototype.onWallCollision = function(robot, up, down, left, right) {
+    robot.turn(60);
+    robot.ahead(30);
+  };
+
+  Robot.prototype.onHitByBullet = function(robot) {
+    robot.disappear();
+    robot.ahead(30);
+    robot.turn(30);
+  };
+  return Robot;
