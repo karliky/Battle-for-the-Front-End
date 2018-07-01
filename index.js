cf7559492c96c5ef6c7a55227fa504c7b2078b14
Carlos Hernández Gómez <carlos.hernandez@socialbro.com>
2018-07-01 22:29:18 +0200

    add saving functionality

---
+++ index.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var path = require('path');
+var fs = require('fs');
+var bodyParser = require('body-parser');
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('./'));
+
+app.post('/save', function(req, res) {
+  const filePath = path.join(__dirname, 'robots', req.body.name + '.js');
+  console.log('New robot saved at ', filePath, req.body.code.length);
+  fs.writeFile(filePath, req.body.code, (err) => {
+    if (err) throw err;
+    res.send({ sucess: true });
+  });
+
+});
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080!');
+});
